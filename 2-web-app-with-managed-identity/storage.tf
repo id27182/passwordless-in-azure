f5b8077497d3b7ac3c396d4942e59b419132c218
Bohdan Mateiko <bohdan.id27182@gmail.com>
2023-06-13 20:56:15 +0300

    disables storage account key authorisation on storage accounts in manages identity auth samples

--- 2-web-app-with-managed-identity/storage.tf
+++ 2-web-app-with-managed-identity/storage.tf
@@ -4,4 +4,8 @@ resource "azurerm_storage_account" "backend" {
     location                 = var.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
+
+    # Disable authorisation using storage account keys -
+    # use Azure AD only
+    shared_access_key_enabled = false
 }
